bec173d6f06f7a83ec0bb2300f7da4b6fcab6784
Tomasz Kramkowski <tk@the-tk.com>
2015-04-04 20:55:41 +0200

    Added more license headers.

--- randtest.c
+++ randtest.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2015  Tomasz Kramkowski <tk@the-tk.com>
+ *
+ * This program is free software. It is licensed under version 3 of the
+ * GNU General Public License.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
