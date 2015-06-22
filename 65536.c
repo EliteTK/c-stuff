365f02290eb174b343e1c1b9409135c58bcba092
EliteTK <tomasz.kramkowski@gmail.com>
2015-06-22 21:07:10 +0100

    65536.c

---
+++ 65536.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <argp.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <error.h>
+#include <errno.h>
+
+struct board {
+	uint8_t tiles[4][4];
+	uint32_t score;
+};
+
+int main(void)
+{
+	return 0;
+}
